7668867a12d7a0e12a52fc14f97fa76cb63bcb3b
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-11-26 01:19:28 +0100

    folder: create folder now works -1

--- app/hooks/useFolders.ts
+++ app/hooks/useFolders.ts
@@ -6,22 +6,19 @@ import { API_URL } from "../config/env";
 const CreateNewFolder = async (name:string)=>{
     const res = await axios.post(`${API_URL}/Folders/create`, {name}, { withCredentials: true });
     return res.data;
-    // api/Folders/create
 }
 
 export const useCreateFolder = () =>{
     return useMutation({
         mutationFn: (name:string)=> CreateNewFolder (name),
         onSuccess: (data) => {
+            console.log(data)
                     toast.success(data.message || 'Folder created successfully!');
-                    // queryClient.invalidateQueries({ queryKey: ['document', id] });
-                    // queryClient.invalidateQueries({ queryKey: ['documents'] });
-                    // queryClient.invalidateQueries({ queryKey: ['user'] });
-        
                 },
                 onError: (error: any) => {
+                    console.log(error?.response)
                     const message =
-                        error?.response?.data?.message || 'Something went wrong while summarizing';
+                        error?.response?.data?.message || 'Failed to create folder. Please try again.';
                     toast.error(message);
                 },
     })
