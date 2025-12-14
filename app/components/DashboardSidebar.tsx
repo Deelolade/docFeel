e6fffdda1d0fabf09fa194333a5c874cf10240cc
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-12-14 01:50:47 +0100

    feature: made the side bar more responsive on dashboard

--- app/components/DashboardSidebar.tsx
+++ app/components/DashboardSidebar.tsx
@@ -10,7 +10,13 @@ import { PiSquaresFourBold } from 'react-icons/pi'
 import { useEffect, useRef, useState } from 'react'
 import { useUserStore } from '../store/userStore'
 import { useDocumentStore } from '../store/documentStore'
+import { IconType } from "react-icons"
 
+type NavLinks = {
+    label: string,
+    link: string,
+    icon: IconType
+}
 const DashboardSidebar = () => {
     const { data: user, isLoading } = useUser();
     const currentUser = { ...user };
@@ -18,10 +24,10 @@ const DashboardSidebar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     const logOutUser = useLogOutUser();
     const logOut = useUserStore(state => state.logOut);
-    const clearStore = useDocumentStore(state=> state.clearStore);
+    const clearStore = useDocumentStore(state => state.clearStore);
 
     // Base styling for all links
-    const linkBaseClasses = "w-full p-3 rounded-lg my-2 flex items-center text-white font-medium transition duration-150 ease-in-out";
+    const linkBaseClasses = "w-full lg:w-full p-2 lg:p-3 rounded-lg my-2 flex items-center text-white font-medium transition duration-150 ease-in-out ";
     const activeClass = "bg-gray-700";
     const inactiveClass = "hover:bg-gray-800";
 
@@ -50,44 +56,70 @@ const DashboardSidebar = () => {
         logOut();
         clearStore()
     }
+    const navigationLinks: NavLinks[] = [
+        { 
+            label: "Dashboard",
+            link: 'dashboard',
+            icon: PiSquaresFourBold
+        },
+        { 
+            label: "Uploads",
+            link: 'uploads',
+            icon: FaRegCaretSquareUp
+        },
+        { 
+            label: "Folders",
+            link: 'folders',
+            icon: FiFolder
+        }
+    ]
+     
     return (
-        <aside className='w-1/5 bg-slate-900 min-h-screen p-6 hidden md:flex flex-col justify-between text-[#EFF6FF] '>
+        <aside className='w-16 lg:w-1/5 bg-slate-900 min-h-screen h-screen px-1 lg:p-6 py-3 hidden md:flex flex-col justify-between text-[#EFF6FF] '>
             {isLoading && <Loading />}
 
             <div className="">
                 <div className=" flex justify-between items-end pb-4 border-b border-slate-800">
-                    <h3 className='text-2xl font-semibold mt-3 text-[#EFF6FF]'>DocFeel</h3>
-                    <p className='text-sm font-semibold text-right'>{currentUser?.trialCount || 0} / 5 trials</p>
+                    <h3 className='text-2xl font-semibold mt-3 text-[#EFF6FF] hidden lg:inline'>DocFeel</h3>
+                    <p className='text-sm font-semibold text-center lg:text-right'>{currentUser?.trialCount || 0} / 5 trials</p>
                 </div>
-                <ul className='mt-12 text-[#EFF6FF]'>
-                    <Link href="/dashboard" className={getLinkClasses("/dashboard")}>
-                        <PiSquaresFourBold className="h-6 w-6 mr-3" /> Dashboard
+                <ul className='mt-12 text-[#EFF6FF] gap-3 md:flex md:flex-col'>
+                    {
+                        navigationLinks && navigationLinks.map((navlinks, idx)=>{
+                             const Icon = navlinks.icon
+                            return(
+                                <Link href={`/${navlinks.link}`} key={idx} className={`${getLinkClasses(`/${navlinks.link}`)} group relative`}>
+                        <Icon className="h-6 w-6 md:size-8 md:mr-0 lg:mr-3 " />
+                        <span className="hidden lg:inline">{navlinks.label}</span>
+                        <span className="pointer-events-none
+      absolute left-full top-1/2 -translate-y-1/2 ml-3
+      whitespace-nowrap
+      rounded-md bg-slate-900 px-3 py-1 text-sm text-white
+      opacity-0 scale-95
+      transition-all duration-200 
+      group-hover:opacity-100 group-hover:scale-100">{navlinks.label}</span>
                     </Link>
-                    <Link href="/uploads" className={getLinkClasses("/uploads")}>
-                        <FaRegCaretSquareUp className="h-6 w-6 mr-3" /> Uploads
-                    </Link>
-                    <Link href="/folders" className={getLinkClasses("/folders")}>
-                        <FiFolder className="h-6 w-6 mr-3" /> Folders
-                    </Link>
-
+                            )
+                        })
+                    }
                 </ul>
             </div>
 
 
-            <div className=" relative flex items-center justify-between mt-8 pt-4 border-t border-slate-800">
-                <div className="flex items-center space-x-3">
-                    <HiOutlineUserCircle className='text-5xl' />
+            <div className=" relative flex items-center justify-center lg:justify-between mt-8 pt-4 border-t border-slate-800">
+                <div className="hidden lg:flex items-center space-x-3 ">
+                    {/* <HiOutlineUserCircle className='text-5xl ' /> */}
                     <div className="">
                         <h4 className='text-lg font-semibold'>{`${currentUser?.name || 'Guest'}`}</h4>
                         <p className='text-sm text-zinc-300 capitalize'>{currentUser.plan || 'Free'} user</p>
                     </div>
                 </div>
                 <button ref={buttonRef} onClick={() => setMenuOpen(!menuOpen)} className='text-xl text-zinc-300 hover:text-white transition duration-150'>
-                    <HiOutlineDotsVertical />
+                    <HiOutlineDotsVertical className='size-6' />
                 </button>
 
                 {menuOpen && (
-                    <div ref={menuRef} className="absolute right-4 bottom-10 mt-2 w-48 bg-slate-800 shadow-lg rounded-lg py-2 z-20">
+                    <div ref={menuRef} className="absolute left-3 bottom-7  lg:right-4 lg:bottom-10 mt-2 w-48 bg-slate-800 shadow-lg rounded-lg py-2 z-20">
                         <button
                             onClick={handleLogOut}
                             className="flex items-center px-4 py-2 text-red-400 hover:text-red-300 hover:bg-red-900/20 w-full text-left"
